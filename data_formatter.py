41fbb10b7e3940c6cf7c566ad00f6611ac967272
Hassan Alamri <alamri.cs@gmail.com>
2020-06-13 15:53:35 -0400

    fixes package deployment (#3)

--- data_formatter.py
+++ data_formatter.py
@@ -2,18 +2,8 @@ from datetime import datetime
 from constants import TWEET_KEYS, USER_KEYS
 
 def format_tweet(tweet): 
-    """ 
-    format tweet 
-  
-    Exclude information, compress dictionaries, and format date
-  
-    Parameters: 
-    tweet (dict): tweet dictionary 
-  
-    Returns: 
-    dict: formatted tweet 
-  
-    """
+    """Exclude information, compress dictionaries, and format date."""
+    # TODO: exlcude urls
     user = tweet['user']['id_str']
     entities = include_keys(tweet['entities'], TWEET_KEYS)
     
@@ -24,76 +14,29 @@ def format_tweet(tweet):
     formatted_tweet['hashtags'] = convert_object_to_array(entities['hashtags'], 'text')
     formatted_tweet['created_at'] = format_date(formatted_tweet['created_at'], '%a %b %d %H:%M:%S +0000 %Y', '%m-%d-%Y %H:%M:%S')
     formatted_tweet['tweet_id'] = formatted_tweet.pop('id_str')
+
     return formatted_tweet
     
 def format_user(tweet): 
-    """ 
-    format user 
-  
-    Exclude information, compress dictionaries, and format date
-  
-    Parameters: 
-    tweet (dict): tweet dictionary 
-  
-    Returns: 
-    dict: formatted user 
-  
-    """
+    """Exclude information, compress dictionaries, and format date."""
     formatted_user = include_keys(tweet['user'], USER_KEYS)
     formatted_user['created_at'] = format_date(formatted_user['created_at'], '%a %b %d %H:%M:%S +0000 %Y', '%m-%d-%Y %H:%M:%S')
     formatted_user['user_id'] = formatted_user.pop('id_str')
     return formatted_user
      
 def include_keys(obj, keys):
-    """ 
-    include Keys 
-  
-    get dictionary with included keys only
-  
-    Parameters: 
-    keys (array): array of keys
-    tweet (dict): tweet dictionary 
-  
-    Returns: 
-    dict: dictionay with only specified keys
-  
-    """
+    """Get dictionary with included keys only."""
     return {x: obj[x] for x in obj if x in keys}
 
 def exlude_url(str):
     return ''
 
 def convert_object_to_array(array, key):
-    """ 
-    convert object to array of values by key
-  
-    get dictionary with included keys only
-  
-    Parameters: 
-    array (array): array of objects
-    key (string): key 
-  
-    Returns: 
-    array: array of values
-  
-    """
+    """Convert object to array of values by key."""
     return [x[key] for x in array]
 
 def format_date(date_str, from_format, to_format):
-    """ 
-    format date to a given format
-  
-    description
-  
-    Parameters: 
-    date_str (string): date in string
-    from_format (string): fromat to convert from 
-    to_format (string): format to convert to 
-
-    Returns: 
-    string: formatted date
-  
-    """
+    """Format date to a given format."""
     x = datetime.strptime(date_str, from_format)
     return x.strftime(to_format)
 
--- handler.py
+++ handler.py
@@ -4,16 +4,18 @@ import data_formatter
 import logging
 from ssm_params_service import get_secret
 from data_access import tweets_db_access, users_db_access
+from sesService import send_email
 from constants import BATCH_SIZE 
 
-LOGGER = logging.getLogger('Handler')
-LOGGER.setLevel(logging.INFO)
-
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
 
 def get_keys():
-    LOGGER.info('Get tweeter account keys')
+    # TODO: PythonDoc
+    log.info('Getting tweeter account keys...')
     tweeter_key = get_secret('tweeter_key')
     splitted_keys = tweeter_key.split(',')
+
     return {
         'api_key': splitted_keys[0],
         'api_secret_key': splitted_keys[1], 
@@ -21,35 +23,46 @@ def get_keys():
         'access_token_secret': splitted_keys[3]
     }
 
-
 def dataCollector(event, context):
+    """ 
+    Lambda handler to collect tweets and persist then in database
+
+    """
     # TODO: Error handling
     # TODO: Handler response
     # TODO: Email notification
     # TODO: Logging
 
-    LOGGER.info('New data collector invocation at')
+    log.info('New data collector invocation at')
     keys = get_keys()
     auth = tweepy.OAuthHandler(keys['api_key'], keys['api_secret_key'])
     auth.set_access_token(keys['access_token'], keys['access_token_secret'])
     api = tweepy.API(auth)
 
     query_str = '(lang:en OR lang:ar) -filter:retweets -filter:replies'
+    log.info('Search tweets with query filters %s' % query_str)
+
     tweets = [tweet._json for tweet in tweepy.Cursor(api.search, q=query_str, tweet_mode='extended').items(100)]
+    log.info('Tweets are retrieved with a length of %d' % len(tweets))
+
     formatted_tweets = [data_formatter.format_tweet(tweet) for tweet in tweets]
     formatted_users = [data_formatter.format_user(tweet) for tweet in tweets]
-    tweets_batches = data_formatter.chunks(formatted_tweets, BATCH_SIZE)
-    users_batches = data_formatter.chunks(formatted_users, BATCH_SIZE)
 
-    for t_batch in tweets_batches:
+    tweet_batches = data_formatter.chunks(formatted_tweets, BATCH_SIZE)
+    user_batches = data_formatter.chunks(formatted_users, BATCH_SIZE)
+
+    for t_batch in tweet_batches:
         tweets_db_access.insert_all(t_batch)
 
-    for u_batch in users_batches:
+    for u_batch in user_batches:
         users_db_access.insert_all(u_batch)
+
+    send_email('Successful invocation', 'Tweets are retrieved with a length of %d' % len(tweets))
 
     return {
         "statusCode": 200,
         "body": "",
     }
 
-dataCollector(1,2)
+if __name__ == "__main__":
+    dataCollector(1,2)
